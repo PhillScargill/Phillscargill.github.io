ef419448f9d24556f81d022fa43f724814ce7074
Jenkum <phill.scargill@gmail.com>
2018-01-10 21:37:54 +0000

    .

--- gfoty/js/main.js
+++ gfoty/js/main.js
@@ -1,73 +1,13 @@
-var interval = 10000;
+var interval = 6000;
 var randPosX;
 
-$(document).on('ready', function() {
-    NewTongue1();
-    NewTongue2();
-    NewTongue3();
-    NewTongue4();
-    NewTongue5();
-    window.setInterval(NewTongue1, interval);
-    window.setInterval(NewTongue2, interval);
-    window.setInterval(NewTongue3, interval);
-    window.setInterval(NewTongue4, interval);
-    window.setInterval(NewTongue5, interval);
-
+$(document).on('ready', function () {
+    NewTongue();
+    window.setInterval(NewTongue, interval);
 })
 
-function NewTongue1(){
-    fallToLeft = false;
-    randPosX = Math.floor((Math.random()*$('#tonguezone').width()));
-    $('#tongueimg')
-        .clone()
-        .css('left', randPosX)
-        .appendTo("#tonguezone")
-        .solitaireVictory();
-}
-
-function NewTongue2(){
-    fallToLeft = true;
-    randPosX = Math.floor((Math.random()*$('#tonguezone').width()));
-    $('#tongueimg')
-        .clone()
-        .css('left', randPosX)
-        .appendTo("#tonguezone")
-        .solitaireVictory();
-}
-
-function NewTongue3(){
-    fallToLeft = false;
-    randPosX = Math.floor((Math.random()*$('#tonguezone').width()));
-    $('#tongueimg')
-        .clone()
-        .css('left', randPosX)
-        .appendTo("#tonguezone")
-        .solitaireVictory();
-}
-
-function NewTongue3(){
-    fallToLeft = true;
-    randPosX = Math.floor((Math.random()*$('#tonguezone').width()));
-    $('#tongueimg')
-        .clone()
-        .css('left', randPosX)
-        .appendTo("#tonguezone")
-        .solitaireVictory();
-}
-
-function NewTongue4(){
-    fallToLeft = false;
-    randPosX = Math.floor((Math.random()*$('#tonguezone').width()));
-    $('#tongueimg')
-        .clone()
-        .css('left', randPosX)
-        .appendTo("#tonguezone")
-        .solitaireVictory();
-}
-
-function NewTongue5(){
-    fallToLeft = true;
-    randPosX = Math.floor((Math.random()*$('#tonguezone').width()));
+function NewTongue() {
+    randPosX = Math.floor((Math.random() * $('#tonguezone').width()));
     $('#tongueimg')
         .clone()
         .css('left', randPosX)
--- gfoty/js/solitaireVictory.js
+++ gfoty/js/solitaireVictory.js
@@ -1,10 +1,10 @@
-(function( $ ) {
+(function ($) {
 
-    $.fn.solitaireVictory = function(settings) {
+    $.fn.solitaireVictory = function (settings) {
 
         settings = settings || {};
 
-        var g = settings.g || -3;
+        var g = settings.g || -5;
         var dt = settings.dt || 20;
         var bounce = settings.bounce || 0.7;
         var endVelocity = settings.endVelocity || 20;
@@ -16,7 +16,7 @@
         var body = $('body');
         var windowHeight = (relativeToDocument ? $(document).height() : $(window).height());
 
-        var fallIteration = function(elem, elemHeight, oldPos, dx, dy) {
+        var fallIteration = function (elem, elemHeight, oldPos, dx, dy) {
             var copy = elem.clone();
             body.append(copy);
 
@@ -28,21 +28,21 @@
             copy.offset(newPos);
             if (Math.abs(newTop - (windowHeight - elemHeight)) < 5) {
                 if (dy < 0 || dy > endVelocity) {
-                    dy *= -1*bounce;
-                    setTimeout(function() {
+                    dy *= -1 * bounce;
+                    setTimeout(function () {
                         fallIteration(copy, elemHeight, newPos, dx, dy);
                     }, dt);
                 }
             } else {
                 dy = dy - g;
-                setTimeout(function() {
+                setTimeout(function () {
                     fallIteration(copy, elemHeight, newPos, dx, dy);
                 }, dt);
             }
         };
 
-        var startFall = function(elem, height, stagger) {
-            var dx = settings.dx || Math.floor((Math.random()*10)) + 5;
+        var startFall = function (elem, height, stagger) {
+            var dx = settings.dx || Math.floor((Math.random() * 10)) + 5;
             if (fallToLeft) {
                 dx = -dx;
             }
@@ -54,21 +54,26 @@
                 copy.css('position', 'fixed');
             }
             var originalOffset = elem.offset();
-            copy.offset({top: originalOffset.top, left: originalOffset.left});
+            copy.offset({
+                top: originalOffset.top,
+                left: originalOffset.left
+            });
             body.append(copy);
-            setTimeout(function() {fallIteration(copy, height, copy.offset(), dx, 0);}, stagger);
+            setTimeout(function () {
+                fallIteration(copy, height, copy.offset(), dx, 0);
+            }, stagger);
         };
 
         if (clear) $('.solitaire-victory-clone').remove();
 
-        this.each(function(index) {
+        this.each(function (index) {
             var obj = $(this);
             if (relativeToDocument || obj.offset().top < $(window).height()) {
                 if (!obj.hasClass('solitaire-victory-clone')) {
-                    startFall(obj, obj.height(), index*stagger);
+                    startFall(obj, obj.height(), index * stagger);
                 }
             }
         });
     };
 
-}( jQuery ));
+}(jQuery));
